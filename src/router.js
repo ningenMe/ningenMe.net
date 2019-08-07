36d01238a76be018581d71fbc031e7d7da7202aa
kuru <amohkuru@gmail.com>
2019-08-08 01:27:59 +0900

    add access

--- src/router.js
+++ src/router.js
@@ -1,34 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
+import TestAccess from './views/TestAccess.vue'
 import Root from './views/Root.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'root',
-      component: Root
-    },
-    {
-      path: '/index.html',
-      name: 'index.html',
-      component: Root
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+            path: '/',
+            name: 'root',
+            component: Root
+        },
+        {
+            path: '/index.html',
+            name: 'index.html',
+            component: Root
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/testaccess',
+            name: 'testaccess',
+            component: TestAccess
+        }
+    ]
 })
